ccd9f62c5ab7ba488a038360bd04effa1c8e3e2d
GuoyongLuo <1660835241@qq.com>
2019-06-10 10:04:36 +0800

    23

---
+++ README.md
@@ -0,0 +1,17 @@
+# 使用第三方ui组件库，快速搭建项目
+
+- Element   pc
+- MintUI    mobile
+- vux       mobile
+- vantui    mobile
+
+# vant 的使用
+
+1. npm install --save vant
+2. 在 main.js 中引入
+
+## 按需引入
+
+1. npm i babel-plugin-import -D
+2. 修改 babel 配置
+3. 删除掉 main.js 中全局引入的方式，改用按需引入的方法。
---
+++ src/utils/http.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { Toast } from 'vant'
+const http = axios.create()
+
+http.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://m.maoyan.com/ajax/' : 'http://m.maoyan.com/ajax/'
+http.defaults.timeout = 10000
+
+http.interceptors.response.use(response => {
+  let res = response.data
+  if (res.status === 0) {
+    // 接口真正成功
+    return res
+  } else {
+    // 接口后台逻辑有bug
+    Toast(res.msg)
+  }
+}, error => {
+  Toast(error.message)
+})
+
+export default http

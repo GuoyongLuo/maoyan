28846941fd32229fb84f30198250e869c4bedbfc
GuoyongLuo <1660835241@qq.com>
2019-06-14 11:36:30 +0800

    film 2

--- vue.config.js
+++ vue.config.js
@@ -15,35 +15,3 @@ module.exports = {
   }
 }
 
-
-// configureWebpack: {
-//   devServer: {
-//       proxy: {
-//           //名字可以自定义，这里我用的是api
-//           '/api': {
-//             target: 'http://m.maoyan.com',//设置你调用的接口域名和端口号 别忘了加http
-//             changeOrigin: true,//这里设置是否跨域
-//             pathRewrite: {
-//               '^/api': ''
-//             }
-//           }
-//       }
-//   }
-// },
-
-
-
-
-// module.exports = {
-
-//   proxy: {
-//         '/apis': {    //将www.exaple.com印射为/apis
-//             target: 'https://www.exaple.com',  // 接口域名
-//             secure: false,  // 如果是https接口，需要配置这个参数
-//             changeOrigin: true,  //是否跨域
-//             pathRewrite: {
-//                 '^/apis': ''   //需要rewrite的,
-//             }              
-//         }
-//   }
-// }
